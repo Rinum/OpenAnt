62ae1ee22c6b9c40425484f2fb110450702afa4d
Rinum <mahmed40@uic.edu>
2011-01-19 16:14:31 -0600

    added Map

---
+++ src/Map.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Open Ant.
+#
+# Open Ant is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Open Ant is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Open Ant.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Map Class
+
+import os
+
+from GLWidget import *
+import Globals
+
+class Map():
+    '''
+    Class for generating maps
+    '''
+    def __init__(self):
+        #Ground tiles
+        self.groundTilesPath = Globals.datadir+'images/ground/'
+        self.groundTiles = []
+
+        #Foliage tiles
+        self.foliageTilesPath = Globals.datadir+'images/foliage/'
+        self.foliageTiles = []
+
+        #Populate list of ground tiles
+        dirList=os.listdir(self.groundTilesPath)
+        for fname in dirList:
+            self.groundTiles.append(fname)
+
+        #Populate list of foliage tiles
+        dirList=os.listdir(self.foliageTilesPath)
+        for fname in dirList:
+            self.foliageTiles.append(fname)
+    
